1b66d9c001060733fdafe7435177926f0e7a45b5
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-01 21:29:24 -0300

    Consulta da média movel de 3 pedidos para cada cliente

--- sql/scripts/02_window_functions.sql
+++ sql/scripts/02_window_functions.sql
@@ -1,1 +1,17 @@
+--1)Para cada cliente, mostre o valor do pedido e o rank dos pedidos (maior → menor).
 
+
+
+--2)Media movel de 3 pedidos para cada cliente
+SELECT
+	customer_id,
+	order_id,
+	dt_pedido,
+	valor_total,
+	ROUND(AVG(valor_total) OVER (
+	PARTITION BY customer_id
+	ORDER BY dt_pedido
+	ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+	), 2
+	) AS media_dos_ultimos_3_pedidos
+FROM orders;
