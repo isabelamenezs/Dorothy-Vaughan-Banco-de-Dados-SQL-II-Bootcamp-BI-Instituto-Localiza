ab4f8c0bef8f071a6b9ef5cb48fed2349768801c
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2025-10-03 14:53:17 -0300

    Update 03_views_temp_tables.sql

--- sql/scripts/03_views_temp_tables.sql
+++ sql/scripts/03_views_temp_tables.sql
@@ -1,1 +1,11 @@
+-- Questão 1) Crie uma View que mostre o faturamento diário consolidado.
 
+CREATE VIEW faturamento_diario AS
+SELECT 
+    dt_pedido,
+    SUM(valor_total) AS faturamento
+FROM orders
+WHERE status = 'Pago'
+GROUP BY dt_pedido;
+
+SELECT * FROM faturamento_diario;
