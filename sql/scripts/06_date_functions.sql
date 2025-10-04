172969838ef9de3ca30878f87ec0afbcb40dc57b
Alana Sampaio <alana.sampaio2000@gmail.com>
2025-10-04 18:57:53 -0300

    inserção questões 6.2 e 8.1

--- sql/scripts/06_date_functions.sql
+++ sql/scripts/06_date_functions.sql
@@ -1,1 +1,7 @@
+-- 1
 
+-- 2 - Calcular quantos dias se passaram desde o pedido
+SELECT * FROM orders;
+SELECT order_id AS order_num, dt_pedido, valor_total, status, ROUND(JULIANDAY(DATE('now')) - JULIANDAY(dt_pedido)) FROM orders;
+
+-- 3
--- sql/scripts/08_case_when.sql
+++ sql/scripts/08_case_when.sql
@@ -1,1 +1,12 @@
+-- 1 - Use CASE WHEN para classificar pedidos em: "Baixo" (<100), "Médio" (100-500), "Alto" (>500).
+SELECT order_id, valor_total, status,
+CASE
+	WHEN valor_total < 100 THEN 'Baixo'
+	WHEN valor_total >= 100 AND valor_total <= 500 THEN 'Médio'
+	WHEN valor_total > 500 THEN 'Alto'
+END AS  classificar_pedidos
+FROM orders;
 
+-- 2
+
+-- 3
