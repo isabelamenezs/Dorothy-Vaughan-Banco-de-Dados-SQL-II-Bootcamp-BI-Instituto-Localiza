5fc30e571724c028826ac535227d57db9a3dcc7f
Vitoria <vitoriaa.academico@gmail.com>
2025-10-04 20:42:09 -0300

    Parte 3 e Parte 4 (Questões 2)

--- sql/scripts/03_views_temp_tables.sql
+++ sql/scripts/03_views_temp_tables.sql
@@ -9,3 +9,17 @@ WHERE status = 'Pago'
 GROUP BY dt_pedido;
 
 SELECT * FROM faturamento_diario;
+
+-- Questão 2) Crie uma Tabela temporária com os clientes que fizeram compras nos últimos 30 dias.
+
+DROP TABLE IF EXISTS clientes_ultimos_30dias;
+
+CREATE TEMP TABLE clientes_ultimos_30dias AS
+SELECT DISTINCT c.customer_id,
+       c.nome
+FROM customers c
+JOIN orders o 
+     ON c.customer_id = o.customer_id
+WHERE o.dt_pedido >= DATE('now', '-30 days');
+
+SELECT * FROM clientes_ultimos_30dias;
--- sql/scripts/04_joins_avancados.sql
+++ sql/scripts/04_joins_avancados.sql
@@ -1,1 +1,15 @@
-
+-- Questão 2) Traga os clientes que compraram todos os produtos de uma categoria (pode usar NOT EXISTS). 
+SELECT c.customer_id, c.nome
+FROM customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM products p
+    WHERE p.categoria = 'Casa'
+      AND NOT EXISTS (
+          SELECT 1
+          FROM orders o
+          JOIN order_items oi ON o.order_id = oi.order_id
+          WHERE o.customer_id = c.customer_id
+            AND oi.product_id = p.product_id
+      )
+);
