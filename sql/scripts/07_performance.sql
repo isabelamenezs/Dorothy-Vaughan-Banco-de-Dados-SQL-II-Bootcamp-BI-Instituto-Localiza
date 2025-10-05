8da1475b3e0ddd30ee6a5555a083b6bb204e4002
laurianoalessa1-byte <lauriano.alessa1@gmail.com>
2025-10-05 17:21:28 -0300

    Update 07_performance.sql

--- sql/scripts/07_performance.sql
+++ sql/scripts/07_performance.sql
@@ -1,1 +1,6 @@
+-- crie um indíce em orders (customer_id, dt_pedido) e explique quando ele será usado
+
+CREATE INDEX idx_orders_customer_date
+ON orders (customer_id, dt_pedido);
+
 -- 3 Identifique consultas que podem ser reescritas usando CTE materializada para melhorar a performance
