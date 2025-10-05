d17023408ca1c5fe8c2e6faf3111368eac0fd1f9
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-05 02:30:51 -0300

    Update 04_joins_avancados.sql

--- sql/scripts/04_joins_avancados.sql
+++ sql/scripts/04_joins_avancados.sql
@@ -13,3 +13,22 @@ WHERE NOT EXISTS (
             AND oi.product_id = p.product_id
       )
 );
+-- Questão 3) Monte um relatório de clientes que compraram um produto, mas nunca outro (ex.: compraram da categoria "Eletrônicos", mas nunca da categoria "Roupas")
+-- A ideia é aqui é fazer especificamente para roupas vs eletronicos, no qual a gente exclui todos que estão com a categoria roupas.
+SELECT DISTINCT
+c.nome
+FROM customers c 
+JOIN orders o ON c.customer_id = o.customer_id 
+JOIN order_items oi ON o.order_id = oi.order_id 
+JOIN products p ON oi.product_id = p.product_id
+WHERE o.status = 'Pago'
+	AND p.categoria = 'Eletrônicos'
+	AND c.customer_id NOT IN (
+	SELECT DISTINCT
+	c2.customer_id 
+	FROM customers c2 
+		JOIN orders o2 ON c2.customer_id = o2.customer_id 
+		JOIN order_items oi2 ON o2.order_id = oi2.order_id 
+		JOIN products p2 ON oi2.product_id = p2.product_id
+		WHERE o2.status = 'Pago'
+		AND p2.categoria = 'Roupas');
