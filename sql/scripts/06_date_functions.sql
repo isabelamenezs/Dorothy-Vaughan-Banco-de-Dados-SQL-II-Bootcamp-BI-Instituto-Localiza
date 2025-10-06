315866ad106cbcb32e3eb7dfec1cd43aeed2055f
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-06 17:08:26 -0300

    Update 06_date_functions.sql
    Exercícios 6.1 e 6.3

--- sql/scripts/06_date_functions.sql
+++ sql/scripts/06_date_functions.sql
@@ -1,9 +1,19 @@
--- 1
+-- 1) Mostrar apenas o ano da data de cadastro dos clientes
+SELECT 
+	c.customer_id,
+	c.nome,
+	STRFTIME('%Y', c.dt_cadastro) AS ano_cadastro
+FROM customers c;
 
--- 2 - Calcular quantos dias se passaram desde o pedido
+-- 2) Calcular quantos dias se passaram desde o pedido
 SELECT * FROM orders;
 SELECT order_id AS order_num, dt_pedido, valor_total, status, 
 ROUND(JULIANDAY(DATE('now')) - JULIANDAY(dt_pedido)) AS dias_passados_desde_pedido
 FROM orders;
 
--- 3
+-- 3) Adicionar 7 dias à data do pedido (ex.: prazo de entrega)
+SELECT
+	o.order_id,
+	o.dt_pedido,
+	DATE(o.dt_pedido, '+7 days') AS prazo_de_entrega
+FROM orders o;
