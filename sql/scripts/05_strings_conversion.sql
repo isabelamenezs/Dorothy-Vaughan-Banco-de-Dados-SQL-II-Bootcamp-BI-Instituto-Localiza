9062c0370958bee9a19a234b2c4c3f6369df70d0
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-06 11:38:08 -0300

    Update 05_strings_conversion.sql
    Adição do código de Nicoli ao documento padrão

--- sql/scripts/05_strings_conversion.sql
+++ sql/scripts/05_strings_conversion.sql
@@ -1,2 +1,13 @@
 
--- 5) Manipulação de Strings e Conversão : Mostre apenas o domínio do e-mail dos clientes (parte depois do @).
+-- 1) Manipulação de Strings e Conversão : Mostre apenas o domínio do e-mail dos clientes (parte depois do @).
+
+
+-- 2) Converta o nome dos clientes para MAIÚSCULAS.
+
+
+-- 3) Crie uma coluna calculada que concatene o nome do cliente com o ID do pedido (ex.: "João - Pedido 123")
+
+SELECT 
+    c.nome || ' - Pedido ' || o.order_id AS cliente_pedido
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id;
