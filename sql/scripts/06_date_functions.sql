dd58b403e18cdf5d690993ba4c1b132224e780e2
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-06 11:41:54 -0300

    Update 06_date_functions.sql

--- sql/scripts/06_date_functions.sql
+++ sql/scripts/06_date_functions.sql
@@ -2,6 +2,8 @@
 
 -- 2 - Calcular quantos dias se passaram desde o pedido
 SELECT * FROM orders;
-SELECT order_id AS order_num, dt_pedido, valor_total, status, ROUND(JULIANDAY(DATE('now')) - JULIANDAY(dt_pedido)) FROM orders;
+SELECT order_id AS order_num, dt_pedido, valor_total, status, 
+ROUND(JULIANDAY(DATE('now')) - JULIANDAY(dt_pedido)) AS dias_passados_desde_pedido
+FROM orders;
 
 -- 3
