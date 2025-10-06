6bb0c2cff618c73bfef22669d807a36fe2457959
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2025-10-06 13:34:34 -0300

    Update 01_cte.sql

--- sql/scripts/01_cte.sql
+++ sql/scripts/01_cte.sql
@@ -1,16 +1,22 @@
 -- Questão 1) Liste o valor médio gasto por cliente usando uma CTE para calcular o total de cada cliente.
 
 WITH total_por_cliente AS (
-  SELECT c.customer_id,
-         c.nome,
-         SUM(o.valor_total) AS total_gasto
+  SELECT 
+    c.customer_id,
+    c.nome,
+    SUM(o.valor_total) AS total_gasto,
+    AVG(o.valor_total) AS valor_medio
   FROM customers c
   JOIN orders o ON c.customer_id = o.customer_id
   GROUP BY c.customer_id, c.nome
 )
-SELECT nome, total_gasto
+SELECT 
+  nome, 
+  total_gasto, 
+  valor_medio
 FROM total_por_cliente
 ORDER BY total_gasto DESC;
+
 
 -- 2. Use uma CTE recursiva para gerar uma sequência de datas (últimos 12 meses) e depois faça um LEFT JOIN com os pedidos para mostrar meses sem vendas.
 
